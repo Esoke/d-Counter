65a11806c2f3d0cf1c69c3213a5f083a77329740
Esma <Blaffer@esmas-mbp.home>
2016-11-20 20:54:18 -0500

    initial commit

--- dCounter/ViewController.swift
+++ dCounter/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var count = 0
+    var mute = false
+    var player: AVAudioPlayer?
+    let MINUS_SOUND = "minusSound"
+    let PLUS_SOUND = "clickSound"
+    
+    @IBOutlet weak var countField: UITextField!
+    
+    @IBOutlet weak var soundButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //--- add UIToolBar on keyboard and Done button on UIToolBar ---//
     }
 
 
+    func playSound(resource:String) {
+        if mute == false{
+            let url = Bundle.main.url(forResource: resource, withExtension: "mp3")!
+        
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                guard let player = player else { return }
+            
+                player.prepareToPlay()
+                player.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        }else{
+            if player != nil {
+                player?.stop()
+                player = nil
+            }
+        }
+    }
+
+    
+    @IBAction func plusPressed(_ sender: Any) {
+        count += 1
+        updateCount()
+        playSound(resource: PLUS_SOUND)
+    }
+
+    @IBAction func minusPressed(_ sender: Any) {
+        count -= 1
+        updateCount()
+        playSound(resource: MINUS_SOUND)
+        
+    }
+    
+    @IBAction func soundPressed(_ sender: Any) {
+        if soundButton.imageView?.image == (UIImage(named: "speaker")){
+            mute = true
+            
+            if let image = UIImage(named: "mute") {
+                soundButton.setImage(image, for: .normal)
+                
+            }
+        }else if soundButton.imageView?.image == (UIImage(named: "mute")){
+            mute = false
+            
+            if let image = UIImage(named: "speaker") {
+                soundButton.setImage(image, for: .normal)
+            }
+        }
+    }
+    
+    func updateCount(){
+        countField.text = String(count)
+    }
+    
+
+       // count = Int(countField.text!) ?? count
+
+    
+    
+    
+    
 }
 
